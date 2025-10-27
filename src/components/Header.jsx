cae2e21261b9f13398d476b06a69ec3ffdf69766
Marysah <Marysah2@gmail.com>
2025-10-27 10:40:44 +0300

    project deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header>
       <div className="header-content">
-        <Link to="/">Marysah's Travel Packing</Link>
+        <Link to="/">Travel Packing</Link>
         <nav>
           <Link to="/">Home</Link>
           {user ? (
